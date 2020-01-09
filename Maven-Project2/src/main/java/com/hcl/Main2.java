2546f3f4354e371d4dc313459c2a6d41b9c2e664
kodurusreekanth25 <sreekanthkoduru25@gmail.com>
2020-01-09 15:30:04 +0530

    hiii

---
+++ Maven-Project2/src/main/java/com/hcl/Main2.java
@@ -0,0 +1,28 @@
+package com.hcl;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AnnotationConfiguration configuration = new AnnotationConfiguration().configure();
+
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+		Session session = sessionFactory.openSession();
+
+		Transaction transaction = session.beginTransaction();
+		Student s1 = new Student();
+		s1.setName("prassu");
+        s1.setAge(21);
+        session.persist(s1);
+        transaction.commit();
+        session.close();
+	}
+
+}
